5e353c724b2fcbce8a252b501a3e2125bf8edb6d
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-02-07 14:02:29 +0100

    [CUSTOM] GRAP: custom company for new created account account

--- openupgrade_scripts/scripts/account/14.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/account/14.0.1.1/post-migration.py
@@ -513,15 +513,30 @@ def fill_account_journal_payment_credit_debit_account_id(env):
                 or journal.company_id.bank_account_code_prefix
                 or ""
             )
+        # GRAP Custom
+        if openupgrade.column_exists(env.cr, "res_company", "fiscal_company_id"):
+            openupgrade.logged_query(
+                env.cr,
+                """
+                SELECT fiscal_company_id from res_company
+                WHERE id = %s""", (journal.company_id.id,),
+            )
+            fiscal_company_id = env.cr.fetchone()[0]
+            company = env["res.company"].search([('id', '=', fiscal_company_id)])
+        else:
+            company = journal.company_id
+
+        # END GRAP CUSTOM
+
         journal.payment_debit_account_id = env["account.account"].create(
             {
                 "name": _("Outstanding Receipts"),
                 "code": env["account.account"]._search_new_account_code(
-                    journal.company_id, digits, liquidity_account_prefix
+                    company, digits, liquidity_account_prefix
                 ),
                 "reconcile": True,
                 "user_type_id": current_assets_type.id,
-                "company_id": journal.company_id.id,
+                "company_id": company.id,
             }
         )
         journal.payment_credit_account_id = (
@@ -530,11 +545,11 @@ def fill_account_journal_payment_credit_debit_account_id(env):
                 {
                     "name": _("Outstanding Payments"),
                     "code": env["account.account"]._search_new_account_code(
-                        journal.company_id, digits, liquidity_account_prefix
+                        company, digits, liquidity_account_prefix
                     ),
                     "reconcile": True,
                     "user_type_id": current_assets_type.id,
-                    "company_id": journal.company_id.id,
+                    "company_id": company.id,
                 }
             )
             .id
@@ -683,9 +698,15 @@ def fill_account_payment_with_no_move(env):
         else:
             p_dates_by_company[p_company] = p_payment_date
     payments = env["account.payment"].browse(list(p_data.keys()))
+    i = 0
     for payment in payments.with_context(
         check_move_validity=False, tracking_disable=True
     ):
+        i+= 1
+        _logger.info(
+            f"{i} / {len(payments)} Create account.move"
+            "for account.payment {payment.id} ..."
+        )
         journal = env["account.journal"].browse(p_data[payment.id]["journal_id"])
         move = env["account.move"].create(
             {
